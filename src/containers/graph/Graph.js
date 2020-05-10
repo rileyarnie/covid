a4e5dcf31fee9b5fe9e95615653d507b04bbc8ff
Arnold Kibira <arnold.ril3y@gmail.com>
2020-05-10 21:28:54 +0300

    add option to change aspect ratio depending on size of screen

--- src/containers/graph/Graph.js
+++ src/containers/graph/Graph.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import { Line } from "react-chartjs-2";
+import styles from "./Graph.module.css";
 
 export class Graph extends Component {
   render() {
     console.log("Graph: ", this.props.dailyData);
     return (
-      <div>
+      <div className={styles.container}>
         <Line
+          options={{ maintainAspectRatio: false }}
           data={{
             labels: this.props.dailyData.map((date) => date.reportDate),
             datasets: [
               {
                 data: this.props.dailyData.map((value) => value.totalConfirmed),
                 backgroundColor: "rgba(22, 22, 196, 0.671)",
-                label: "Infected",
+                label: "Infections",
                 borderColor: "blue",
               },
               {
