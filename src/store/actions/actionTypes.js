5092363c64af5e20c65fea44336cb87bd0ed893c
Arnold Kibira <arnold.ril3y@gmail.com>
2020-05-10 21:31:47 +0300

    define the get daily action type and retrieve the date

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,39 @@
+import * as actionTypes from "./actions";
+import axios from "axios";
+
+const BASE_URL = "https://covid19.mathdro.id/api/";
+
+export const gettingData =()=> (dispatch) => {
+
+    let url = BASE_URL
+
+  axios
+    .get(url)
+    .then((res) => {
+      dispatch(getData(res.data));
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getData = (data) => {
+  return {
+    type: actionTypes.GET_DATA,
+    payload: data,
+  };
+};
+
+export const gettingDailyData = () =>dispatch=>{
+
+    axios.get(`${BASE_URL}daily/`).then(res=>{
+        dispatch(getDailyData(res.data))
+    }).catch(err=>console.log(err))
+
+}
+
+export const getDailyData=(data)=>{
+    return{
+        type: actionTypes.GET_DAILY_DATA,
+        payload: data
+    }
+}
+
