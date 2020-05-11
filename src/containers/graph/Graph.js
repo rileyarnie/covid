12cb1b5a8643b16d2a7ef71fe507b0d59e698bd1
Arnold Kibira <arnold.ril3y@gmail.com>
2020-05-11 11:14:18 +0300

    add favicon

--- src/containers/graph/Graph.js
+++ src/containers/graph/Graph.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Line, Pie, Bar } from "react-chartjs-2";
+import { Line, Pie } from "react-chartjs-2";
 import styles from "./Graph.module.css";
 
 export class Graph extends Component {
