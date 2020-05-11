9bd4304ff68771775c6b946aad8c04da62122f48
Arnold Kibira <arnold.ril3y@gmail.com>
2020-05-11 10:49:33 +0300

    render either Line graph or pie chart depending on user choice

--- src/containers/graph/Graph.js
+++ src/containers/graph/Graph.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import { Line } from "react-chartjs-2";
+import { Line, Pie, Bar } from "react-chartjs-2";
 import styles from "./Graph.module.css";
 
 export class Graph extends Component {
   render() {
-    console.log("Graph: ", this.props.dailyData);
-    return (
+    const lineChart = this.props.dailyData ? (
       <div className={styles.container}>
         <Line
           options={{ maintainAspectRatio: false }}
@@ -20,6 +19,7 @@ export class Graph extends Component {
               },
               {
                 data: this.props.dailyData.map((value) => value.deaths.total),
+
                 backgroundColor: "rgba(240, 12, 12, 0.747)",
                 label: "Deaths",
                 borderColor: "red",
@@ -28,7 +28,40 @@ export class Graph extends Component {
           }}
         />
       </div>
+    ) : null;
+    const pieChart = (
+      <div>
+        <Pie className={styles.pieChart}
+          data={{
+            labels: ["Infected", "Recovered", "Deaths"],
+            datasets: [
+              {
+                label: "People",
+                backgroundColor: [
+                  "rgba(22, 22, 196, 0.671)",
+                  "rgba(75, 236, 11, 0.671)",
+                  "rgba(240, 12, 12, 0.747)",
+                ],
+                data: [
+                  this.props.globalData.confirmed.value,
+                  this.props.globalData.recovered.value,
+                  this.props.globalData.deaths.value,
+                ],
+              },
+            ],
+          }}
+          options={{
+            legend: { display: false },
+            title: {
+              display: true,
+              text: `Situation in ${this.props.country}`,
+            },
+          }}
+        />
+      </div>
     );
+
+    return <>{this.props.country ? pieChart : lineChart}</>;
   }
 }
 
