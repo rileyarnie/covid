0373f20eb51b3d27587823e9a8c2d7ce5186b0a9
Arnold Kibira <arnold.ril3y@gmail.com>
2020-05-11 20:31:09 +0300

    fix spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { AppBar, Toolbar, Typography } from "@material-ui/core";
 class Header extends Component {
   render() {
     return (
-      <div className="mt-3">
+      <div>
         <AppBar position='static'>
           <Toolbar>
             <Typography variant="h6">COVID-19 UPDATES</Typography>
